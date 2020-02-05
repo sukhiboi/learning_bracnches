5d4e991902e9d1cd8f69ae5937a20d61edc54faf
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-02-05 12:22:02 +0530

    added function isOdd

--- calc.js
+++ calc.js
@@ -2,9 +2,10 @@ const mul = require('./src/multiply');
 const div = require('./src/divide');
 const add = require('./src/add');
 const sub = require('./src/subtract');
+const isOdd = require('./src/isOdd');
 
 const main = function(args) {
-  const calcTools = { add, mul, div, sub };
+  const calcTools = { add, mul, div, sub, isOdd };
   console.log(calcTools[args[0]](+args[1], +args[2]));
 };
 
---
+++ src/isOdd.js
@@ -0,0 +1,5 @@
+const isOdd = function(num) {
+  return num % 2 !== 0;
+};
+
+module.exports = isOdd;
