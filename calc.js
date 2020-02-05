90a1b7ac415659e060d57c944b9eb838acb166cd
rivu <58026024+unphydra@users.noreply.github.com>
2020-02-05 12:18:53 +0530

    added:isEven

--- calc.js
+++ calc.js
@@ -1,4 +1,6 @@
 const multiply = require('./src/multiply');
 const subtract = require('./src/subtract');
+const isEven = require('./src/isEven');
 
 console.log(multiply(2, 3), subtract(5, 2));
+console.log(isEven(6));
