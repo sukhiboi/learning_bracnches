042af986b5d3fe70847853b665ade0b18bbe0b24
sukhiboi <41sukhdevsingh@gmail.com>
2020-02-05 12:10:30 +0530

    Created percent function

--- calc.js
+++ calc.js
@@ -1,6 +1,8 @@
 const multiply = require('./src/multiply');
 const subtract = require('./src/subtract');
 const add = require('./src/add');
+const percent = require('./src/percent');
 
 console.log(multiply(2, 3), subtract(5, 2));
 console.log(add(3, 4));
+console.log(percent(10, 90));
