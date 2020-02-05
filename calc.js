446b0c8f8a7657b163928c16ed94637e37cedb06
sukhiboi <41sukhdevsingh@gmail.com>
2020-02-05 12:24:52 +0530

    Created fibo function

--- calc.js
+++ calc.js
@@ -1,6 +1,8 @@
 const multiply = require('./src/multiply');
 const subtract = require('./src/subtract');
 const add = require('./src/add');
+const fibo = require('./src/fibo');
 
 console.log(multiply(2, 3), subtract(5, 2));
 console.log(add(3, 4));
+console.log(fibo(6));
