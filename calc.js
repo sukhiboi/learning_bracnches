dabcbd214b5a9777d84412bfc0ce037e2f4fbb38
rivu <58026024+unphydra@users.noreply.github.com>
2020-02-05 12:13:12 +0530

    added:factorila

--- calc.js
+++ calc.js
@@ -1,4 +1,6 @@
 const multiply = require('./src/multiply');
 const subtract = require('./src/subtract');
+const factorial = require('./src/factorial');
 
 console.log(multiply(2, 3), subtract(5, 2));
+console.log(factorial(5));
