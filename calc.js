059709401b5b756fed7e9eb26af4152a896d3067
sukhiboi <41sukhdevsingh@gmail.com>
2020-02-05 11:59:04 +0530

    Merged with rivu and used add function

--- calc.js
+++ calc.js
@@ -1,4 +1,6 @@
 const multiply = require('./src/multiply');
 const subtract = require('./src/subtract');
+const add = require('./src/add');
 
 console.log(multiply(2, 3), subtract(5, 2));
+console.log(add(3, 4));
