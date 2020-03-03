aa16d0f77cbd48a2719c120ca54db24594fb31d2
rivu <58026024+unphydra@users.noreply.github.com>
2020-03-04 00:13:54 +0530

    added space

--- calc.js
+++ calc.js
@@ -2,10 +2,11 @@ const mul = require('./src/multiply');
 const div = require('./src/divide');
 const add = require('./src/add');
 const percent = require('./src/percent');
+
 const sub = require('./src/subtract');
 
 const main = function(args) {
-  const calcTools = { add, mul, div, sub, percent};
+  const calcTools = { add, mul, div, sub, percent };
   console.log(calcTools[args[0]](+args[1], +args[2]));
 };
 
