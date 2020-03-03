c2bb1b86ccb15dda5ee0fefefe7d1f3510a82c1e
rivu <58026024+unphydra@users.noreply.github.com>
2020-03-03 12:41:22 +0530

    rebase

--- calc.js
+++ calc.js
@@ -3,10 +3,15 @@ const div = require('./src/divide');
 const add = require('./src/add');
 const percent = require('./src/percent');
 const sub = require('./src/subtract');
+const multiply = require('./src/multiply');
+const subtract = require('./src/subtract');
+const factorial = require('./src/factorial');
 
 const main = function(args) {
-  const calcTools = { add, mul, div, sub, percent};
+  const calcTools = { add, mul, div, sub, percent };
   console.log(calcTools[args[0]](+args[1], +args[2]));
+  console.log(multiply(2, 3), subtract(5, 2));
+  console.log(factorial(5));
 };
 
 main(process.argv.slice(2));
